350d1fecdf517182f7a0a0ec0f275cbe802d3c90
Gokul <gokulsrinivask2020@gmail.com>
2023-07-29 12:53:30 +0530

    rotate_list

---
+++ Linked_List/rotate_list.cpp
@@ -0,0 +1,37 @@
+
+//   Definition for singly-linked list.
+//   struct ListNode {
+//       int val;
+//       ListNode *next;
+//       ListNode() : val(0), next(nullptr) {}
+//       ListNode(int x) : val(x), next(nullptr) {}
+//       ListNode(int x, ListNode *next) : val(x), next(next) {}
+//   };
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || k==0 || head->next==NULL){
+            return head;
+        }
+        ListNode* curr = head;
+        int count=1;
+        while(curr->next!=NULL){
+            curr=curr->next;
+            count++;
+        }
+        k = k%count;
+        k=count-k;
+        curr->next=head;
+        while(k--){
+            curr=curr->next;
+
+        }
+        head=curr->next;
+        curr->next=NULL;
+        return head;
+            
+    }
+};
+ 
+
